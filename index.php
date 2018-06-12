e528b2078216359b7bf948e840b6fd97c980135d
miavila <miavila@me.com>
2018-06-12 15:23:08 +0200

    Prova

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+define('BASE_DIR','C:/AppServ/www/po/');
+
+/* buscar msgid  */
+$array = file("en_EN1.po");
+foreach ($array as $mystring){
+	$buscar = 'msgid "';
+	$pos = strpos($mystring, $buscar);
+	if ($pos!==FAlSE) {
+		$remplazado = str_replace("msgid ", "", $mystring);
+		$resultadoId .= $remplazado;
+	 }
+}
+echo '<pre>';
+// print_r($resultadoId);
+
+
+
+/* array traduciones */
+$traduccions = file("mia_es.txt");
+
+
+/* remplazar traduciones */
+$array = file("en_EN1.po");
+$i = 0;
+$p= 0;
+foreach ($array as $mystring){
+	$replaceOriginal = array("msgstr[0]", "msgstr[1]");
+	$replaceFinal = array ("msgstr", "");
+	$mystring = str_replace($replaceOriginal, $replaceFinal, $mystring);
+
+	$buscar = 'msgstr "';
+	$plural = 'msgid_';
+	$pos = strpos($mystring, $buscar);
+	$plural = strpos($mystring, $plural);
+	if ($plural!==FAlSE) {
+		$mystring = '';
+	}
+	if ($pos!==FAlSE) {
+		$traduccio = $traduccions[$i];
+		$mystring = 'msgstr '.$traduccio.'';
+		$i++;
+	}
+	 print_r($mystring);
+}
